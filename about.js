1d35644bf75aab1dd0d18356c87634e349c16f67
sonu995 <114069856+sonu995@users.noreply.github.com>
2023-11-24 14:39:03 +0530

    Rcth

---
+++ about.js
@@ -0,0 +1,102 @@
+var lang = {
+    "html": "100%",
+    "css": "90%",
+    "javascript": "70%",
+    "php": "55%",
+    "angular": "65%"
+  };
+  
+  var multiply = 4;
+  
+  $.each( lang, function( language, pourcent) {
+  
+    var delay = 700;
+    
+    setTimeout(function() {
+      $('#'+language+'-pourcent').html(pourcent);
+    },delay*multiply);
+    
+    multiply++;
+  
+  });
+
+
+  
+
+  let nav = document.querySelector(".navigation-wrap");
+  window.onscroll = function(){
+      if(document.documentElement.scrollTop > 20){
+          nav.classList.add("scroll-on")
+      }
+      else{
+          nav.classList.remove("scroll-on")
+  
+      }
+  }
+
+
+
+  const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+
+
+var lowerSlider = document.querySelector('#lower');
+var  upperSlider = document.querySelector('#upper');
+
+document.querySelector('#two').value=upperSlider.value;
+document.querySelector('#one').value=lowerSlider.value;
+
+var  lowerVal = parseInt(lowerSlider.value);
+var upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+
+    if (upperVal < lowerVal + 4) {
+        lowerSlider.value = upperVal - 4;
+        if (lowerVal == lowerSlider.min) {
+        upperSlider.value = 4;
+        }
+    }
+    document.querySelector('#two').value=this.value
+};
+
+lowerSlider.oninput = function () {
+    lowerVal = parseInt(lowerSlider.value);
+    upperVal = parseInt(upperSlider.value);
+    if (lowerVal > upperVal - 4) {
+        upperSlider.value = lowerVal + 4;
+        if (upperVal == upperSlider.max) {
+            lowerSlider.value = parseInt(upperSlider.max) - 4;
+        }
+    }
+    document.querySelector('#one').value=this.value
+};
+
+
+
---
+++ script.js
@@ -0,0 +1,115 @@
+var lang = {
+    "html": "100%",
+    "css": "90%",
+    "javascript": "70%",
+    "php": "55%",
+    "angular": "65%"
+  };
+  
+  var multiply = 4;
+  
+  $.each( lang, function( language, pourcent) {
+  
+    var delay = 700;
+    
+    setTimeout(function() {
+      $('#'+language+'-pourcent').html(pourcent);
+    },delay*multiply);
+    
+    multiply++;
+  
+  });
+
+
+
+  import { CountUp } from 'https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js';
+
+function countStart() {
+  const $counters = document.querySelectorAll(".js-count-up"),
+  options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ",",
+    decimal: "." };
+
+
+  $counters.forEach(item => {
+    const value = item.dataset.value;
+    const counter = new CountUp(item, value, options);
+    counter.start();
+  });
+}
+
+new Waypoint({
+  element: document.querySelector('.level'),
+  handler: function () {
+    countStart();
+    //this.destroy() //for once
+  },
+  offset: '50%' });
+
+
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:15,
+        nav:true,
+        autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+               
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+  });;
+
+  
+
+  let nav = document.querySelector(".navigation-wrap");
+  window.onscroll = function(){
+      if(document.documentElement.scrollTop > 20){
+          nav.classList.add("scroll-on")
+      }
+      else{
+          nav.classList.remove("scroll-on")
+  
+      }
+  }
+
+
+
+  const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
